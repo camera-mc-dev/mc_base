453977a9aca89e1bf0074d2c1caa7297a1ea6796
Murray Evans <m.evans@bath.ac.uk>
2023-07-27 09:38:33 +0100

    mocap pipeline install scripts

---
+++ cloneMocapRepos.sh
@@ -0,0 +1,4 @@
+git clone git@github.com:camera-mc-dev/mc_core
+git clone git@github.com:camera-mc-dev/mc_sds
+git clone git@github.com:camera-mc-dev/mc_reconstruction
+git clone git@github.com:camera-mc-dev/mc_opensim
